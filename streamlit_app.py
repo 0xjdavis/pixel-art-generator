afb1639c4af97e1a4f9798ad198253d1d4b4e6c6
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-15 21:48:38 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,27 @@ import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
 
+st.sidebar.header("About App")
+st.sidebar.write("This is an app to convert rasterized images (jpeg, png) to a pixelized format by 0xjdavis.")
+
+# Calendly
+st.sidebar.markdown("""
+    <hr />
+    <center>
+    <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+    <br />
+    <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br />
+    <a href="https://calendly.com/0xjavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+    </div>
+    </center>
+    <br />
+""", unsafe_allow_html=True)
+
+# Copyright
+st.sidebar.caption("©️ Copyright 2024 J. Davis")
+
+
 st.title('Generate Pixelized Art')
 st.subheader('Covert your jpg/png images in to a pixelized version.')
 
